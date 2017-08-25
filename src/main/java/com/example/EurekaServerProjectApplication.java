9d943b0bb75867785cd553e4d309e79c2326237c
Devops <devopsInabox@newtglobal.com>
2017-08-25 20:06:26 +0530

    Update EurekaServerProjectApplication.java

--- src/main/java/com/example/EurekaServerProjectApplication.java
+++ src/main/java/com/example/EurekaServerProjectApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerProjectApplication {
-	
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerProjectApplication.class, args);
