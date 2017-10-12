8224b21d9d003506fe6ab51092a597706bdfcfb1
Devops <devopsInabox@newtglobal.com>
2017-10-12 12:23:31 +0530

    Update EurekaServerProjectApplication.java

--- src/main/java/com/example/EurekaServerProjectApplication.java
+++ src/main/java/com/example/EurekaServerProjectApplication.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-
 @SpringBootApplication
 @EnableEurekaServer
 @EnablePrometheusEndpoint
